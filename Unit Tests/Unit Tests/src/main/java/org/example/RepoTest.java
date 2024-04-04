527de2344e4fbd5751eee4c6a9e5f084b8b8c7e4
hubertleszcz <hubciol1308@gmail.com>
2024-04-04 23:22:46 +0200

    asdmasjfnakfa

--- Unit Tests/Unit Tests/src/main/java/org/example/RepoTest.java
+++ Unit Tests/Unit Tests/src/main/java/org/example/RepoTest.java
@@ -1,11 +1,12 @@
 package org.example;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RepoTest {
     private MageRepository repository;
@@ -17,6 +18,47 @@ public class RepoTest {
         repository = new MageRepository();
         mage = new Mage("Ten karzeł z Jackassów", 2);
 
+    }
+    @Test
+    @DisplayName("Test zapisu maga")
+    void testSave(){
+        assertDoesNotThrow(()->repository.save(mage));
+    }
+
+    @Test
+    @DisplayName("Test zapisu istniejącego maga")
+    void testExisteingSave(){
+        repository.save(mage);
+        Mage mage2 = new Mage("Ten karzeł z Jackassów", 2);
+        assertThrows(IllegalArgumentException.class,()->repository.save(mage2));
+    }
+
+    @Test
+    @DisplayName("Test odczytu maga")
+    void testRead(){
+        repository.getCollection().add(mage);
+        Optional<Mage> check = repository.find(mage.getName());
+        Assertions.assertTrue(check.isPresent());
+        Assertions.assertEquals(mage, check.get());
+    }
+
+    @Test
+    @DisplayName("Test odczytu nieistniejącego maga")
+    void testUnexistingRead(){
+        Optional<Mage> check = repository.find(mage.getName());
+        Assertions.assertTrue(check.isEmpty());
+    }
+    @Test
+    @DisplayName("Test usunięcia maga")
+    void testDelete(){
+        repository.getCollection().add(mage);
+        Assertions.assertDoesNotThrow(()->repository.delete(mage.getName()));
+    }
+
+    @Test
+    @DisplayName("Test usunięcia nieistniejącego maga")
+    void testUnexistingDelete(){
+        assertThrows(IllegalArgumentException.class, ()->repository.delete(mage.getName()));
     }
 
     @AfterEach
